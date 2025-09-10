197d89bf6e61fbd5c3f67ccf9c7c4e5e666d5d44
PrasoonKumar9759 <pirsoonkumar@gmail.com>
2025-09-11 01:23:59 +0530

    adding daily ques

---
+++ minimumFallingPAthSumII.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        int[][] dp=new int[matrix.length][matrix[0].length];
+        for(int[] a:dp){
+            Arrays.fill(a,-66666);
+        }
+        int ans=Integer.MAX_VALUE;
+        for(int col=0;col<matrix[0].length;col++){
+            ans=Math.min(ans,minimumFall(matrix,0,col,dp));
+        }
+        return ans;
+    }
+    public static int minimumFall(int[][] matrix,int cr,int cc,int[][] dp){
+        if(cr==matrix.length-1){
+            return matrix[cr][cc];
+        }
+        if(dp[cr][cc]!=-66666){
+            return dp[cr][cc];
+        }
+        int ans=Integer.MAX_VALUE;
+        for(int i=0;i<matrix[0].length;i++){
+            if(i==cc){
+                continue;
+            }
+            ans=Math.min(ans,minimumFall(matrix,cr+1,i,dp));
+        }
+
+        return dp[cr][cc]=ans+matrix[cr][cc];
+    }
+}
